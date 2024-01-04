088f006bbd598b4ec89625387216d2ce2f55f32f
cclewis18 <155507116+cclewis18@users.noreply.github.com>
2024-01-04 04:47:31 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,44 @@
+// Create web server application that can accept a POST request
+// with a JSON payload and store the payload in a file.
+// The JSON payload should contain a key called comment and
+// the value should be a string.
+// The JSON payload should be stored in a file called comments.json
+// in the same directory as the server application.
+// The file should contain one JSON object per line.
+// Each JSON object should have a key called comment and the value
+// should be the value from the JSON payload.
+// The server should respond with a 200 status code and a JSON payload
+// with a key called message and the value should be "success".
+// If the JSON payload does not contain a key called comment,
+// the server should respond with a 400 status code and a JSON payload
+// with a key called message and the value should be "error".
+// If the JSON payload is not valid JSON, the server should respond
+// with a 400 status code and a JSON payload with a key called message
+// and the value should be "error".
+
+// Create web server application that can accept a GET request
+// and respond with a JSON payload that contains all of the comments
+// that have been stored in the file.
+// The server should respond with a 200 status code.
+// If there are no comments in the file, the server should respond
+// with a 200 status code and an empty JSON array.
+
+// Create web server application that can accept a DELETE request
+// and respond with a JSON payload that contains all of the comments
+// that have been stored in the file.
+// The server should respond with a 200 status code.
+// If there are no comments in the file, the server should respond
+// with a 200 status code and an empty JSON array.
+
+// Create web server application that can accept a PUT request
+// with a JSON payload and store the payload in a file.
+// The JSON payload should contain a key called comment and
+// the value should be a string.
+// The JSON payload should be stored in a file called comments.json
+// in the same directory as the server application.
+// The file should contain one JSON object per line.
+// Each JSON object should have a key called comment and the value
+// should be the value from the JSON payload.
+// The server should respond with a 200 status code and a JSON payload
+// with a key called message and the value should be "success".
+//
